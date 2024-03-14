f0dedf839db8647992d953dc9246e1cb93d12c36
seva <garanarsen@gmail.com>
2024-03-14 20:27:56 +0200

    feat: added exceptions to maintain basic errors

---
+++ src/main/java/com/iGainsTwo/iGainsJ/exceptions/UserNeverExistedException.java
@@ -0,0 +1,7 @@
+package com.iGainsTwo.iGainsJ.exceptions;
+
+public class UserNeverExistedException extends Exception{
+    public UserNeverExistedException(String message) {
+        super(message);
+    }
+}
