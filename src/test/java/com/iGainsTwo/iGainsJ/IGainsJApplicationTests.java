d9d4eb0b16bbc3941ca9e9ba4297187c23643704
seva <garanarsen@gmail.com>
2024-03-14 18:19:18 +0200

    init commit

---
+++ src/test/java/com/iGainsTwo/iGainsJ/IGainsJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iGainsTwo.iGainsJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IGainsJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
